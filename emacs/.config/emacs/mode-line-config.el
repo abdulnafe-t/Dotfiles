138967320e6f73d275386d1b52281e98191cd8ae
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-03-05 15:35:23 +0100

    Enable active/inactive window differentiation in the modeline.
    Add which-function support in the modeline.

--- emacs/.config/emacs/mode-line-config.el
+++ emacs/.config/emacs/mode-line-config.el
@@ -1,38 +1,30 @@
 ;; -*- lexical-binding: t; -*-
 ;; Based in large part on https://emacs.stackexchange.com/a/16658
 
-(defun strip-backend-advice (file &optional backend)
-  (when (stringp vc-mode)
-    (let ((gitlogo (replace-regexp-in-string "^ Git." "  " vc-mode)))
-      (setq vc-mode gitlogo))))
-
-(advice-add 'vc-mode-line :after #'strip-backend-advice)
+(defun scion/format-git-string-advice (file &optional backend)
+  "Strip VC mode prefix and apply face based on file state."
+  (when (and (buffer-file-name)
+             (vc-backend (buffer-file-name)))
+    (let* ((state (vc-state (buffer-file-name)))
+           (face (pcase state
+                   ((or 'up-to-date 'nil) 'vc-state-base)
+                   ('edited 'vc-edited-state)
+                   ('needs-merge 'vc-edited-state)
+                   ('needs-update 'vc-edited-state)
+                   ('added 'vc-edited-state)
+                   ('removed 'vc-edited-state)
+                   ('conflict 'vc-edited-state)
+                   ('locked 'vc-locked-state)
+                   (_ 'vc-state-base)))
+           (gitlogo (concat (propertize "  " 'display '(raise 0.1)
+                                         'face face)
+                           (replace-regexp-in-string "^ Git:" "  " vc-mode))))
+    (setq vc-mode gitlogo))))
+(advice-add 'vc-mode-line :after #'scion/format-git-string-advice)
 
 (setopt mode-line-position-column-line-format '("%l:%c"))
 
-(defun mode-line-fill-right (face reserve)
-  "Return empty space using FACE and leaving RESERVE space on the right."
-  (unless reserve
-    (setq reserve 20))
-  (when (and window-system (eq 'right (get-scroll-bar-mode)))
-    (setq reserve (- reserve 3)))
-  (propertize " "
-              'display `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))
-              'face face))
-
-
-(defun mode-line-fill-center (face reserve)
-  "Return empty space using FACE to the center of remaining space leaving RESERVE space on the right."
-  (unless reserve
-    (setq reserve 20))
-  (when (and window-system (eq 'right (get-scroll-bar-mode)))
-    (setq reserve (- reserve 3)))
-  (propertize " "
-              'display `((space :align-to (- (+ center (.5 . right-margin)) ,reserve
-                                             (.47 . left-margin))))
-              'face face))
-
-(defun scion-buffer-name-with-project ()
+(defun scion/buffer-name-with-project ()
   "Display buffer name as Project|file."
   (cond
    (buffer-file-name
@@ -46,12 +38,33 @@
                             (directory-file-name project-root))))
            (file-name (file-name-nondirectory buffer-file-name)))
       (if project-name
-          (concat project-name "|" (propertize file-name 'face '(bold mode-line-buffer-id)))
+          (concat project-name ": " (propertize file-name 'face '(bold mode-line-buffer-id)))
         (propertize file-name 'face '(bold mode-line-buffer-id)))))
    (t
     (propertize (buffer-name) 'face '(bold mode-line-buffer-id)))))
 
-(defconst RIGHT_PADDING 0)
+(defun mode-line-fill-right (face reserve)
+  "Return empty space using FACE and leaving RESERVE space on the right."
+  (unless reserve
+    (setq reserve 20))
+  (when (and window-system (eq 'right (get-scroll-bar-mode)))
+    (setq reserve (- reserve 3)))
+  (propertize " "
+              'display `((space :align-to (- (+ right right-fringe right-margin) ,reserve)))
+              'face face))
+
+(defun mode-line-fill-center (face reserve)
+  "Return empty space using FACE to the center of remaining space leaving RESERVE space on the right."
+  (unless reserve
+    (setq reserve 20))
+  (when (and window-system (eq 'right (get-scroll-bar-mode)))
+    (setq reserve (- reserve 3)))
+  (propertize " "
+              'display `((space :align-to (- (+ center (.5 . right-margin)) ,reserve
+                                             (.46 . left-margin))))
+              'face face))
+
+(defconst RIGHT_PADDING -1)
 
 (defun reserve-left/middle ()
   (/ (length (format-mode-line mode-line-align-middle)) 2))
@@ -71,33 +84,35 @@
         mode-line-frame-identification
         (:eval
          (when (mode-line-window-selected-p)
-           (concat "    " (format-mode-line mode-line-position) (propertize (format "(%d:%d)" (count-lines (point-min) (point-max)) fill-column) 'face 'shadow) "  ")))))
+           (concat "    " (format-mode-line mode-line-position) (propertize (format "(%d:%d)" (count-lines (point-min) (point-max)) fill-column) 'face 'shadow) "  ")))
+
+        (:eval (when (and which-function-mode which-func-format (mode-line-window-selected-p))
+                 (concat "     " (format-mode-line which-func-format) "     ")))
+
+        ))
 
 (setq mode-line-align-middle
       '(""
         (project-mode-line project-mode-line-format)
-        (:eval (concat (format-mode-line vc-mode vc-mode) "   "))
-        (:eval (scion-buffer-name-with-project))
+        (:eval (concat (format-mode-line vc-mode vc-mode) "    "))
+        (:eval (scion/buffer-name-with-project))
         ))
 
 (setq mode-line-align-right
       '(""
         flymake-mode-line-format
-        mode-line-misc-info
-        (:eval (when (mode-line-window-selected-p) mode-name))
+
+        (:eval (when (eglot--managed-mode) "  Eglot  "))
+        (:eval  (format-mode-line mode-name))
         ))
 
 (setq-default mode-line-format
               (list
-
                mode-line-align-left
-
-               '(:eval (mode-line-fill-center 'mode-line
+               '(:eval (mode-line-fill-center (if (mode-line-window-selected-p) 'mode-line-active 'mode-line-inactive)
                                               (reserve-left/middle)))
-
                mode-line-align-middle
-               '(:eval
-                 (mode-line-fill-right 'mode-line
-                                       (reserve-middle/right)))
+               '(:eval (mode-line-fill-right (if (mode-line-window-selected-p) 'mode-line-active 'mode-line-inactive)
+                                             (reserve-middle/right)))
                mode-line-align-right
                ))
