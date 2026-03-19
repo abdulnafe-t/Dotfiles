66d56ce6b87f253f6845b167045a23a403f3748b
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-03-19 10:32:32 +0100

    Subscribe to emacshorrors.com in elfeed

--- emacs/.config/emacs/elfeed-config.el
+++ emacs/.config/emacs/elfeed-config.el
@@ -17,18 +17,20 @@
 
   (setopt elfeed-feeds
 	  '(("https://xkcd.com/rss.xml" xkcd humor general comics)
-	    ("https://blog.ar-ms.me/atom.xml" abdul-rahman-sibahi arabic software typography general)
 	    ("https://archlinux.org/feeds/news" arch linux software)
+            ("https://emacshorrors.com/feed.atom" emacs software humor)
+	    ("https://blog.ar-ms.me/atom.xml" abdul-rahman-sibahi arabic software typography general)
 	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCGudGqToOw9hWGR88W9Jutg" youtube howtown)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw" youtube bread-on-penguins arch linux life general)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCD6VugMZKRhSyzWEWA9W2fg" youtube ssethtzeentach gaming)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCX7lrgEOku1Ii5smnxDS5uQ" youtube soju-with-sarah podcast life)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UChzRJQ-MbpcIxFT5YLW1R9w" youtube juniper-dev gamedev gaming)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCX60pqsaaAPFh2sUZEaNKJA" youtube HGModernism life tech art general)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCSdma21fnJzgmPodhC9SJ3g" youtube nakey-jakey gaming)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UC8e0Sg8TmRRFJytjEGhmVTg" youtube rhystic-studies mtg art)
 	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCYO_jab_esuFRV4b17AJtAw" youtube 3blue1brown math)
-	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCkcnYVAVZQOB-nXHechtXDg" youtube benjamin finance))))
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCkcnYVAVZQOB-nXHechtXDg" youtube benjamin finance)
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCSdma21fnJzgmPodhC9SJ3g" youtube nakey-jakey gaming)
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCD6VugMZKRhSyzWEWA9W2fg" youtube ssethtzeentach gaming)
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UC8e0Sg8TmRRFJytjEGhmVTg" youtube rhystic-studies mtg art)
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UChzRJQ-MbpcIxFT5YLW1R9w" youtube juniper-dev gamedev gaming)
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCX7lrgEOku1Ii5smnxDS5uQ" youtube soju-with-sarah podcast life)
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCX60pqsaaAPFh2sUZEaNKJA" youtube HGModernism life tech art general)
+	    ("https://www.youtube.com/feeds/videos.xml?channel_id=UCwHwDuNd9lCdA7chyyquDXw" youtube bread-on-penguins arch linux life general)
+            )))
 
 (use-package elfeed-tube ;; improves youtube feeds
   :after elfeed
