5e821c74f2c564f973da635e2aed367fddff37c2
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-03-28 16:58:35 +0100

    Speed up startup time

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,5 +1,32 @@
 ;;; -*- lexical-binding: t -*-
 
+;; From
+;; https://www.reddit.com/r/emacs/comments/yzb77m/an_easy_trick_i_found_to_improve_emacs_startup/
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
+(defvar config:file-name-handler-alist-cache file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(defun config:restore-post-init-settings ()
+  (setq gc-cons-threshold 800000
+        gc-cons-percentage 0.1)
+  (setq file-name-handler-alist config:file-name-handler-alist-cache))
+(add-hook 'emacs-startup-hook #'config:restore-post-init-settings)
+
+(defun config:defer-gc ()
+  (setq gc-cons-threshold most-positive-fixnum))
+(defun config:-do-restore-gc ()
+  (setq gc-cons-threshold 800000))
+(defun config:restore-gc ()
+  (run-at-time 1 nil #'config:-do-restore-gc))
+
+(add-hook 'minibuffer-setup #'config:defer-gc)
+(add-hook 'minibuffer-exit #'config:restore-gc)
+
+(setq auto-mode-case-fold nil
+      process-adaptive-read-buffering t
+      inhibit-compacting-font-caches t
+      inhibit-startup-message t)
+
 ;; Ensure package.el uses the elpa directory under this user-emacs-directory
 (setq package-user-dir (expand-file-name "elpa/" user-emacs-directory))
 
