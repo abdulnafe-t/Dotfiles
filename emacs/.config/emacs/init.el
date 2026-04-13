762499cbd348e3ffb5b98e08016dc7968444497c
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-04-13 11:14:34 +0200

    Add rust-mode

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1099,12 +1099,12 @@
      "/home/scion/Projects/Notepad--"))
  '(package-selected-packages
    '(agent-shell auctex avy beginend consult ef-themes eglot eldoc-box elfeed elfeed-tube
-                 embark embark-consult expand-region forge highlight-doxygen hydra jinx
-                 json-mode lorem-ipsum magit magit-prime marginalia markdown-mode minions
-                 move-text multiple-cursors nerd-icons-completion nerd-icons-dired
-                 no-littering olivetti orderless org-appear org-bullets page-break-lines
-                 pdf-tools posframe pulsar svg-lib typescript-mode vertico vundo
-                 whole-line-or-region wiki-summary ws-butler yasnippet))
+		 embark embark-consult expand-region forge highlight-doxygen hydra jinx
+		 json-mode lorem-ipsum magit magit-prime marginalia markdown-mode minions
+		 move-text multiple-cursors nerd-icons-completion nerd-icons-dired
+		 no-littering olivetti orderless org-appear org-bullets page-break-lines
+		 pdf-tools posframe pulsar rust-mode svg-lib typescript-mode vertico vundo
+		 whole-line-or-region wiki-summary ws-butler yasnippet))
  '(send-mail-function 'smtpmail-send-it))
 
 ;; ## added by opam user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
